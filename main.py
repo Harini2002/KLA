7e81861505050abf3928436b6d34f270df432e57
Harini2002 <85167703+Harini2002@users.noreply.github.com>
2022-03-08 14:01:03 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -9,32 +9,38 @@ def value(d,fn):
         if type(d[i]) is dict:
             if d[i]!= 'Activities':
                 for k,v in d[i].items():
+
                    if type(v) is dict and  v!='Inputs':
+                       arr.append(k)
                        value(v,fn)
                    if 'Inputs' in v:
                        input_values=v['Inputs']
                        exc_delay=input_values['ExecutionTime']
                        time.sleep(int(exc_delay))
                        filename=input_values['FunctionInput']
-                       logger.info('%s. %s Entry', fn,k)
-                       logger.info('%s. %s Executing DataLoad (%s)',fn, k,filename)
-                       logger.info('Exit')
+                       str = ".".join(arr)
+                       logger.info('%s Entry',str)
+                       logger.info('%s Executing DataLoad (%s)',str,filename)
+                       logger.info('%s Exit',str)
+                       arr.pop()
 
 
 
-logging.basicConfig(filename="output.log",
+logging.basicConfig(filename="output2.log",
                     format='%(asctime)s %(message)s',
                     filemode='w')
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
-with open('Milestone1A.yaml') as f:
+with open('Milestone1B.yaml') as f:
     data = yaml.load(f, Loader=SafeLoader)
-imp=""
+
+arr=[]
 for i in data:
-    imp=imp+i
-    logger.info('%s Entry',imp)
-    value(data[i],imp)
-logger.info('%s Exit',imp)
+    logger.info('%s Entry',i)
+    arr.append(i)
+    value(data[i],arr)
+
+logger.info('%s Exit',arr[0])
 
 
 
