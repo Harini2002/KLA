6905dc845548652b65176bc46961942871cf323b
Harini2002 <85167703+Harini2002@users.noreply.github.com>
2022-03-08 13:09:07 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
 import yaml
 from yaml.loader import SafeLoader
+
+import time
+import logging
+
+def value(d,imp):
+    for i in d:
+        if type(d[i]) is dict:
+            if d[i]!= 'Activities':
+                for k,v in d[i].items():
+                    if 'Inputs' in v:
+                       input_values=v['Inputs']
+                       exc_delay=input_values['ExecutionTime']
+                       time.sleep(int(exc_delay))
+                       input_file=input_values['FunctionInput']
+                    logger.info('imp. %s Entry', k)
+                    logger.info('imp. %s Executing DataLoad(%s)', k,input_file)
+                    logger.info('Exit')
+
+logging.basicConfig(filename="output.log",
+                    format='%(asctime)s %(message)s',
+                    filemode='w')
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 with open('Milestone1A.yaml') as f:
     data = yaml.load(f, Loader=SafeLoader)
 
-list_of_keys={}
+
+
+imp=""
 for i in data:
-        if type(data[i])=="<class 'dict'>":
-            list_of_keys[i]=1
-            list_of_keys[values(data[i])]=1
-print(list_of_keys)
+    imp=imp+i
+    logger.info('%s Entry',imp)
+    value(data[i],imp)
+
+logger.info('%s Exit',imp)
 
 
-
-
-
-def values(d):
-    for j in d:
-        if type(d[j])=="<class 'dict'>":
-            values(d)
-            return( d[j])
 
 
 
