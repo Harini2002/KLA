6559fd5158bfe802458b65b18edba4aab9bb24b0
Harini2002 <85167703+Harini2002@users.noreply.github.com>
2022-03-08 14:09:12 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -19,28 +19,28 @@ def value(d,fn):
                        time.sleep(int(exc_delay))
                        filename=input_values['FunctionInput']
                        str = ".".join(arr)
-                       logger.info('%s Entry',str)
-                       logger.info('%s Executing DataLoad (%s)',str,filename)
-                       logger.info('%s Exit',str)
+                       logger.info(';%s Entry',str)
+                       logger.info(';%s Executing TimeFunction (%s,%s)',str,filename,exc_delay)
+                       logger.info(';%s Exit',str)
                        arr.pop()
 
 
 
-logging.basicConfig(filename="output2.log",
+logging.basicConfig(filename="output.log",
                     format='%(asctime)s %(message)s',
                     filemode='w')
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
-with open('Milestone1B.yaml') as f:
+with open('Milestone1A.yaml') as f:
     data = yaml.load(f, Loader=SafeLoader)
 
 arr=[]
 for i in data:
-    logger.info('%s Entry',i)
+    logger.info(';%s Entry',i)
     arr.append(i)
     value(data[i],arr)
 
-logger.info('%s Exit',arr[0])
+logger.info(';%s Exit',arr[0])
 
 
 
